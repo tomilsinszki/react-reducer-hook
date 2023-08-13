18c570ca191393eafad39620c6c7c8254d2a465c
Tom Ilsinszki <ilsinszkitamas@gmail.com>
2023-08-13 14:33:21 +0200

    implement login screen

---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            You are logged in!
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Login.tsx
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+
+const Login = (props: {loginHandler(username : string, password : string) : void}) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        props.loginHandler(username, password);
+    };
+
+    const usernameChangedHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.value.trim() === '') {
+            return;
+        }
+
+        setUsername(event.target.value);
+    };
+
+    const passwordChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.value.trim() === '') {
+            return;
+        }
+
+        setPassword(event.target.value);
+    };
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div>
+                <input type='text' value={username} onChange={usernameChangedHandler} />
+            </div>
+            <div>
+                <input type='password' value={password} onChange={passwordChangeHandler} />
+            </div>
+            <div>
+                <button type='submit'>Login</button>
+            </div>
+        </form>
+    );
+};
+
+export default Login;

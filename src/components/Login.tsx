0265bf9a1b8a5210544bddf05f6895232709f764
Tom Ilsinszki <ilsinszkitamas@gmail.com>
2023-08-13 20:47:25 +0200

    using reducer

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,29 +1,59 @@
-import React, {useState} from 'react';
+import React, {useEffect, useReducer, useState} from 'react';
+
+const usernameReducer = (state : any, action : any) => {
+    if (action.type === 'USER_INPUT') {
+        return { value: action.value, isValid: action.value.length > 3 };
+    }
+
+    return { value: '', isValid: false };
+};
+
+const passwordReducer = (state : any, action: any) => {
+    if (action.type === 'USER_INPUT') {
+        return { value: action.value, isValid: action.value.length > 8 };
+    }
+
+    return { value: '', isValid: false };
+};
 
 const Login = (props: {loginHandler(username : string, password : string) : void}) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const [usernameState, dispatchUsername] = useReducer(usernameReducer, { value: '', isValid: false });
+    const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: '', isValid: false });
+    const [formIsValid, setFormIsValid] = useState(false);
+
+    const { isValid: isUsernameValid } = usernameState;
+    const { isValid: isPasswordValid } = passwordState;
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setFormIsValid(usernameState.isValid && passwordState.isValid)
+        });
+
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [isUsernameValid, isPasswordValid]);
 
     const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        props.loginHandler(username, password);
+        props.loginHandler(usernameState.value, passwordState.value);
     };
 
     const usernameChangedHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setUsername(event.target.value);
+        dispatchUsername({type: 'USER_INPUT', value: event.target.value});
     };
 
     const passwordChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(event.target.value);
+        dispatchPassword({type: 'USER_INPUT', value: event.target.value});
     };
 
     return (
-        <form onSubmit={submitHandler}>
-            <div>
-                <input type='text' value={username} onChange={usernameChangedHandler} />
+        <form onSubmit={submitHandler} style={{ padding: '20px', borderColor: formIsValid ? 'green' : 'red', borderWidth: '2px', borderStyle: 'solid' }}>
+            <div style={{ padding: '3px', borderColor: usernameState.isValid ? 'green' : 'red', borderWidth: '2px', borderStyle: 'solid' }}>
+                <input type='text' value={usernameState.value} onChange={usernameChangedHandler} />
             </div>
-            <div>
-                <input type='password' value={password} onChange={passwordChangeHandler} />
+            <div style={{ padding: '3px', borderColor: passwordState.isValid ? 'green' : 'red', borderWidth: '2px', borderStyle: 'solid' }}>
+                <input type='password' value={passwordState.value} onChange={passwordChangeHandler} />
             </div>
             <div>
                 <button type='submit'>Login</button>

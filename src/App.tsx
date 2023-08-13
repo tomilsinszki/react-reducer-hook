c97b93d25345e41e3e2dd55b3b8b4c6de39b6989
Tom Ilsinszki <ilsinszkitamas@gmail.com>
2023-08-13 18:06:05 +0200

    add useEffect comments

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,38 @@ import Home from "./components/Home";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // RUNS AFTER EACH COMPONENT RELOAD
+  /*
+  useEffect(() => {
+    console.log('RUNS AFTER EACH COMPONENT RELOAD');
+  });
+  */
+
+  // ONLY RUNS ONCE AFTER THE FIRST TIME THE COMPONENT WAS LOADED
+  // because the dependency array is [] and so the empty array does not change
+  /*
+  useEffect(() => {
+    console.log('ONLY RUNS ONCE AFTER THE FIRST TIME THE COMPONENT WAS LOADED');
+  }, []);
+  */
+
+  // RUNS AFTER THE FIRST TIME THE COMPONENT WAS LOADED AND AFTER isLoggedIn CHANGES
+  /*
+  useEffect(() => {
+    console.log('RUNS AFTER THE FIRST TIME THE COMPONENT WAS LOADED + WHEN isLoggedIn CHANGES');
+  }, [isLoggedIn]);
+  */
+
+  /*
+  useEffect(() => {
+    console.log('RUNS AFTER THE FIRST TIME THE COMPONENT WAS LOADED + WHEN isLoggedIn CHANGES');
+
+    return () => {
+      console.log('RUNS BEFORE EACH RELOAD - BUT NOT BEFORE THE ORIGINAL RELOAD')
+    };
+  }, [isLoggedIn]);
+  */
+
   useEffect(() => {
     const storedIsLoggedIn = localStorage.getItem('isLoggedIn');
 
